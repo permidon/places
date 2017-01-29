20615650386e6803c0f7476157dacf1e35fa8e55
Roman Perminov <permidon@me.com>
2017-01-29 17:51:43 -0500

    Create Places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,25 @@ class PlacesController < ApplicationController
   def index
   end
 
+  def new
+    @place = Place.new
+  end
+
+  def create
+    @place = Place.new(place_params)
+    @place.save
+    redirect_to @place
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address1, :address2, :sity,
+                                  :state, :zipcode, :country, :phone, :email)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :places
   root 'places#index'
 
   match '/about_us',   to: 'pages#about_us',   via: :get
