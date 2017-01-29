cfaafb36aaf697f51cc610a84b9cb991bbe65822
Roman Perminov <permidon@me.com>
2017-01-29 17:20:33 -0500

    Pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+
+  def about_us
+
+  end
+
+  def contacts_us
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 
   root 'places#index'
 
+  match '/about_us',   to: 'pages#about_us',   via: :get
+  match '/contact_us', to: 'pages#contact_us', via: :get
+
 end
