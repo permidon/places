f84cd79022858987f64c895a7513fc7966e3843e
Roman Perminov <permidon@me.com>
2017-01-30 21:13:59 -0500

    Dummy Data 1

--- db/schema.rb
+++ db/schema.rb
@@ -52,11 +52,10 @@ ActiveRecord::Schema.define(version: 20170131012251) do
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,27 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Category.where(name: "Hotel").first_or_create(name: "Hotel")
-Category.where(name: "Restaurant").first_or_create(name: "Restaurant")
-Category.where(name: "Cafe").first_or_create(name: "Cafe")
-Category.where(name: "Bar").first_or_create(name: "Bar")
-Category.where(name: "Club").first_or_create(name: "Club")
-Category.where(name: "Store").first_or_create(name: "Store")
+hotel = Category.where(name: "Hotel").first_or_create(name: "Hotel")
+restaurant = Category.where(name: "Restaurant").first_or_create(name: "Restaurant")
+cafe = Category.where(name: "Cafe").first_or_create(name: "Cafe")
+bar = Category.where(name: "Bar").first_or_create(name: "Bar")
+pub = Category.where(name: "Pub").first_or_create(name: "Pub")
+club = Category.where(name: "Club").first_or_create(name: "Club")
+store = Category.where(name: "Store").first_or_create(name: "Store")
+theatre = Category.where(name: "Theatre").first_or_create(name: "Theatre")
+
+Place.where(name: "Taco Bell").first_or_create(name: "Taco Bell", description: "tacos", city: "saint paul", state: "mn", category_id: restaurant.id)
+
+Place.where(name: "Park Inn").first_or_create(name: "Park Inn", description: "sleep", city: "los angeles", state: "ca", category_id: hotel.id)
+
+Place.where(name: "Wave").first_or_create(name: "Wave", description: "coffee", city: "miami", state: "fl", category_id: cafe.id)
+
+Place.where(name: "Drink").first_or_create(name: "Drink", description: "beer", city: "new york", state: "ny", category_id: bar.id)
+
+Place.where(name: "Irish Pub").first_or_create(name: "Irish Pub", description: "pale ale", city: "new york", state: "ny", category_id: pub.id)
+
+Place.where(name: "Studio 54").first_or_create(name: "Studio 54", description: "snow", city: "new york", state: "ny", category_id: club.id)
+
+Place.where(name: "BestBuy").first_or_create(name: "BestBuy", description: "electronic", city: "new york", state: "ny", category_id: store.id)
+
+Place.where(name: "Madison Square Garden").first_or_create(name: "Madison Square Garden", description: "musical", city: "new york", state: "ny", category_id: theatre.id)
