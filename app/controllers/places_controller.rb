34fbb5801b8548e701bd2b81bd7fd25968272336
Roman Perminov <permidon@me.com>
2017-02-01 21:49:55 -0500

    Admin Authorization

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,7 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new]
+  before_action :authenticate_user!, only: [:create, :new, :edit, :update, :destroy]
+  before_action :set_place, only: [:show, :edit, :update, :destroy]
+  before_action :is_admin?, only: [:edit, :update, :destroy]
 
   def index
     # visitor_latitude = request.location.latitude
@@ -28,7 +30,7 @@ class PlacesController < ApplicationController
   end
 
   def show
-    @place = Place.find(params[:id])
+
     @reviews = Review.where(place_id: @place)
     if @reviews.blank?
       @avg_rating = 0
@@ -37,13 +39,26 @@ class PlacesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @place.update(place_params)
+    redirect_to @place
+  end
+
+  def destroy
+    @place.destroy
+    redirect_to root_path
+  end
+
+
   def search
     if params[:category].blank?
       @places = Place.all
     else
       @places = Place.search(params)
     end
-
   end
 
   private
@@ -51,6 +66,17 @@ class PlacesController < ApplicationController
   def place_params
     params.require(:place).permit(:name, :description, :address1, :address2, :city, :image,
                                   :state, :zipcode, :country, :phone, :email, :category_id)
+  end
+
+  def set_place
+    @place = Place.find(params[:id])
+  end
+
+  def is_admin?
+    if !current_user.try(:admin?)
+      redirect_to root_path
+      flash[:danger] = "You are not authorized to edit or delete."
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170131012251) do
+ActiveRecord::Schema.define(version: 20170202015414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 20170131012251) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -56,10 +56,11 @@ ActiveRecord::Schema.define(version: 20170131012251) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.boolean  "admin",                  default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
